5b36afe5d364371c0c37677f59ef963ac58532f5
Muhammad Yusuf <mohdyusufr@gmail.com>
2021-11-13 18:09:56 +0530

    Add p7

---
+++ Day_13/p7.py
@@ -0,0 +1,15 @@
+# Which number do you want to check?
+
+# number = int(input()) 
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+number = int(input()) 
+
+if number % 2 == 0: #assignment was incorrect
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
