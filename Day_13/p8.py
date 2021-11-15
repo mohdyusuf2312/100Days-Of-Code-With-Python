be71ad18016e4472c49946ea786b2bf5c9efa635
Muhammad Yusuf <mohdyusufr@gmail.com>
2021-11-15 18:10:31 +0530

    Add p8

---
+++ Day_13/p8.py
@@ -0,0 +1,28 @@
+# Which year do you want to check?
+
+year = input()
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+
+# leapyear problem debugged input was in str not in int
+year = int(input())
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
