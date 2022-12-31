f4fd47ffe88ac6c9069651bb2ee6846b0e22a6c5
Muhammad Yusuf <mohdyusufr@gmail.com>
2022-12-31 15:31:12 +0530

    Add p5

---
+++ Day_13/p5.py
@@ -0,0 +1,15 @@
+#Print is Your Friend
+
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words == pages * word_per_page
+# print(total_words)
+
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
