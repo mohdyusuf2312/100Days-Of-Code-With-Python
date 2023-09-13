9486d601731e68eb21725f61ac967e7d3d4f7061
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-09-13 15:29:25 +0530

    remove typing mistake

--- Day_13/p4.py
+++ Day_13/p4.py
@@ -2,7 +2,7 @@
 
 # age = int(input("How old are you?"))
 # if age > 18:
-#     print("You can drive at age {age}.")
+# print("You can drive at age {age}.")
 
 age = int(input("How old are you?"))
 if age > 18:
