17d05c6c1d7b901d5041d47ea95ddbbbe8d87fc1
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-03-27 21:34:10 +0530

    Add Day_6

---
+++ Day_6/Lec_62Function.py
@@ -0,0 +1,28 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def single_jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+   
+# for s in range(0,4):
+#     single_jump()
+
+# def pr():
+#  print("x")
+
+# pr()
+
+#  print("hgdh")
+# A space before print instruction gives error
