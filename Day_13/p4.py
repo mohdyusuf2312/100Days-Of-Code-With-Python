77c8788a894530fb6f8ca5ea38b70eb4159b0777
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-04-05 10:30:26 +0530

    Add p4.py

---
+++ Day_13/p4.py
@@ -0,0 +1,9 @@
+# Fix the Errors
+
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print("You can drive at age {age}.")
+
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"You can drive at age {age}.")
