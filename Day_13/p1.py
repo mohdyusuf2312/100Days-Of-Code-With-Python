ae5e0d628a796ea6f3e9703aba18dbe2f8ad69cc
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-07-03 10:01:10 +0530

    P2.py

--- Day_13/p1.py
+++ Day_13/p1.py
@@ -2,6 +2,13 @@
 
 # Describe Problem
 def my_function():
+  for i in range(1, 20):
+    if i == 20:
+      print("You got it")
+my_function()
+
+
+def my_function():
   for i in range(1, 21):
     if i == 20:
       print("You got it")
