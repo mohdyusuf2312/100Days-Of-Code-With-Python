fece8d5768ade262b956654477afbbc2209505ee
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-07-03 10:13:59 +0530

    Add p3

---
+++ Day_13/p3.py
@@ -0,0 +1,14 @@
+# Play Computer
+
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year > 1994:
+#   print("You are a Gen Z.")
+
+
+year = int(input("What's your year of birth?"))
+if year > 1980 and year <= 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
