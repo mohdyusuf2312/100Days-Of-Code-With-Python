aea5dc83de787bc845f81d75c78e1ee0cf79f03e
MOHD YUSUF <145057367+mohdyusuf2312@users.noreply.github.com>
2025-08-08 12:23:00 +0530

    Update and rename Lec_34LearYear.py to Lec_34LeapYear.py

--- Day_03/Lec_34LeapYear.py
+++ Day_03/Lec_34LeapYear.py
@@ -12,7 +12,7 @@ year = int(input("Enter year : "))
 
 #Alternate
 
-if ((year % 4 == 0) or (year % 100 == 0))and year % 400 == 0:
+if ((year % 4 == 0) and (year % 100 != 0)) or year % 400 == 0:
     print(f"{year} is a leap year.")
 else:
     print(f"{year} is not a leap year.")
